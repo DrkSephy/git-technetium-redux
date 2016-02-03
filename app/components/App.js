f8ab6a1cc5dc7fb72d43e4d67c8f36bf6d6a94fa
DrkSephy <dleonar00@citymail.cuny.edu>
2016-02-03 18:34:37 -0500

    Implemented Navbar component

--- app/components/App.js
+++ app/components/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Navbar from './Navbar'
 
 class App extends React.Component {
   render() {
     return (
       <div>
+        <Navbar history={this.props.history} />
         {this.props.children}
       </div>
     );
