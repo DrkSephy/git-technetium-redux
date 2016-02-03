436c88f7739f78692f729c7786863ae5426c2179
DrkSephy <dleonar00@citymail.cuny.edu>
2016-02-03 15:17:50 -0500

    Parsing contribution data in commits controller

--- controllers/commits.js
+++ controllers/commits.js
@@ -2,10 +2,26 @@
 
 module.exports = (app, request) => {
   app.get('/api/commits', (req, res) => {
-    request.get('https://api.github.com/repos/DrkSephy/git-technetium/contributors', 
-      { 'headers': {'User-Agent': 'DrkSephy' }},
-      (error, response, body) => {
-        res.send(body);
+    getJSON('https://api.github.com/repos/DrkSephy/git-technetium/contributors')
+    .then((data) => {
+      let parsedJSON = [];
+      data.forEach((entry) => {
+        let contributionData = {};
+        contributionData.username = entry.login;
+        contributionData.contributions = entry.contributions;
+        parsedJSON.push(contributionData);
       });
+      res.send(parsedJSON);
+    });
   });
+
+  function getJSON(url) {
+    return new Promise((resolve, reject) => {
+      request.get(url, { 'headers': {'User-Agent': 'DrkSephy' }}, 
+        (error, response, body) => {
+          const data = JSON.parse(body);
+          resolve(data);
+        });
+    });
+  }
 }
