171f09e6390f78edd0912dc379704f60d86e1dd7
DrkSephy <dleonar00@citymail.cuny.edu>
2016-02-03 15:49:56 -0500

    Forgot to add routes into main.js

--- app/main.js
+++ app/main.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Router from 'react-router';
 import ReactDOM from 'react-dom';
 import createBrowserHistory from 'history/lib/createBrowserHistory';
+import routes from './routes';
 
 let history = createBrowserHistory();
 
