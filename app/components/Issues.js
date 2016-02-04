baf6b73e3b1e879cd835171fa26e532311fd6e32
DrkSephy <dleonar00@citymail.cuny.edu>
2016-02-03 19:50:26 -0500

    Implemented Issue Component, Store, Action and linked to Navbar component

---
+++ app/components/Issues.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import {Link} from 'react-router';
+import IssuesStore from '../stores/IssuesStore';
+import IssuesActions from '../actions/IssuesActions';
+
+class Issues extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = IssuesStore.getState();
+    this.onChange = this.onChange.bind(this);
+  }
+
+  componentDidMount() {
+    IssuesStore.listen(this.onChange);
+    IssuesActions.getIssues();
+  }
+
+  componentWillUnmount() {
+    IssuesStore.unlisten(this.onChange);
+  }
+
+  onChange(state) {
+    this.setState(state);
+  }
+
+  render() {
+    let issues = this.state.issues.map((data) => {
+      return (
+        <tr key={data.id}>
+          <td>{data.title}</td>
+          <td>{data.number}</td>
+          <td>{data.username}</td>
+          <td>{data.state}</td>
+        </tr>
+      );
+    });
+
+    return (
+      <div className='container'>
+        <div className='panel panel-default'>
+          <table className='table table-striped'>
+            <thead>
+              <tr>
+                <td colSpan='1'>Title</td>
+                <td colSpan='1'>Number</td>
+                <td colSpan='1'>Username</td>
+                <td colSpan='1'>State</td>
+              </tr>
+            </thead>
+            <tbody>
+              {issues}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Issues;
--- app/routes.js
+++ app/routes.js
@@ -3,10 +3,12 @@ import {Route} from 'react-router';
 import App from './components/App';
 import Home from './components/Home';
 import Contributions from './components/Contributions';
+import Issues from './components/Issues';
 
 export default (
   <Route component={App}>
     <Route path='/' component={Home} />
     <Route path='/contributions' component={Contributions} />
+    <Route path='/issues' component={Issues} />
   </Route>
 );
